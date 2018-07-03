ec3752eccce3dbf2b581cd94316fa82f097e64a0
Javad <bc_alimohammadi@yahoo.com>
2018-07-04 00:26:55 +0430

    fix source and target compiler plugin

--- para-server/src/main/java/com/erudika/para/ParaServer.java
+++ para-server/src/main/java/com/erudika/para/ParaServer.java
@@ -292,10 +292,6 @@ public class ParaServer implements WebApplicationInitializer, Ordered {
 	 * @param args args
 	 */
 	public static void main(String[] args) {
-		System.setProperty("http.proxyHost", "127.0.0.1");
-		System.setProperty("https.proxyHost", "127.0.0.1");
-		System.setProperty("http.proxyPort", "8888");
-		System.setProperty("https.proxyPort", "8888");
 
 		runAsJAR(args, ParaServer.class);
 	}
